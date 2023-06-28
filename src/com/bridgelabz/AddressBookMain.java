2283a8e973f6b50e0baa9b2051d81e36d72cfdc3
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-06-28 21:09:29 +0530

    Added : Contact of the person

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book Program");
+        AddressBook FamilyBook = new AddressBook();
+        FamilyBook.addContact();
+    }
+}
