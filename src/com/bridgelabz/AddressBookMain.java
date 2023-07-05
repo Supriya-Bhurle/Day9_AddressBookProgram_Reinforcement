19cd1e5f27909eba35830559377fef8b3dea1a61
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-07-05 14:51:53 +0530

    Added : Ability to add multiple address book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,40 +1,48 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class AddressBookMain {
-    static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        AddressBook inputMethods = new AddressBook();
-        System.out.println("Welcome to the address Book Program!!");
-       // System.out.println("How many Person you want to add in address book:");
-      //  int person = scanner.nextInt();
-       // Contacts[] array = new Contacts[person];
+public class AddressBookMain extends AddressBook{
+    public static Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Contacts> array = new ArrayList<>();
+    public static void main(String[] args) {
+        System.out.println("Welcome to the address Book Program!!");
+        HashMapClass hashMapClass = new HashMapClass();
+        AddressBook inputMethods = new AddressBook();
+        AddressBookMain addressBook = new AddressBookMain();
+        HashMap<String, ArrayList<Contacts>> hashMap = new HashMap<>();
         int result =0;
         while(result!=4) {
             System.out.println();
-            System.out.println("Select the option from following Menu");
-            System.out.println("1. Add" + " 2. Edit" + " 3. Print" + " 4. Delete" + " 5. Stop");
+            System.out.println("What do you want to perform from Main Menu");
+            System.out.println("1. Add\n2. Edit\n3. Delete\n4.List Of Address Book\n5.Print specific Book\n6. Print complete address Book\n7. Search\n8. Stop");
             result = scanner.nextInt();
             switch (result) {
                 case 1:
-                    inputMethods.add(array);
+                    hashMapClass.addBook(hashMap);
                     break;
                 case 2:
-                    inputMethods.edit(array);
+                    hashMapClass.toEdit(hashMap);
                     break;
-
-                case 3 :
-                    inputMethods.print(array);
+                case 3:
+                    hashMapClass.toDeleteBook(hashMap);
+                    break;
                 case 4:
-                    inputMethods.delete(array);
+                    hashMapClass.listOfBookNames(hashMap);
                     break;
                 case 5:
-                    System.out.println("Thank You!!");
+                    hashMapClass.particularBookName(hashMap);
                     break;
+                case 6 :
+                    hashMapClass.printMap(hashMap);
+                    break;
+                case 7 :
+                    hashMapClass.searchBookWithName(hashMap);
+                    break;
+                case 8 :
+                    System.out.println("Thank You !!");
                 default:
                     System.out.println("Invalid input!!");
             }
